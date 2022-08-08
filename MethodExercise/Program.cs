d898546582c4dee510a56bd9a6dde23b6e3bdb1b
Eddie Z <e.zarate7@hotmail.com>
2022-08-08 09:28:22 -0600

    initial commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Nice to meet you"); 
+                Console.WriteLine(name);
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+            Console.WriteLine("Thats awesome you like");
+            Console.WriteLine(band);
+
+
+
+
+
+
+
+
+        }
+    }
+}
